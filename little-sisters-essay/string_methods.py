f5dde792ae7f46c51d6f01e5fe78c151db4bb68c
oscar p <oscarperezm16@hotmail.com>
2021-10-06 23:49:36 -0500

    little

---
+++ little-sisters-essay/string_methods.py
@@ -0,0 +1,12 @@
+def capitalize_title(title):
+ 
+    return title.title()
+def check_sentence_ending(sentence):
+   
+    return sentence[-1] == '.'
+def clean_up_spacing(sentence):
+  
+    return sentence.strip()
+def replace_word_choice(sentence, old_word, new_word):
+   
+    return sentence.replace(old_word, new_word)
